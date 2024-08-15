397f286ac73995a742ee5ed00e821f9b53fb653e
yppelu <yppelu@gmail.com>
2024-08-15 13:10:48 +0700

    Merge app.gets

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.get(['/', '/home'], middleware);
-app.get('/about', middleware);
-app.get('/contact-me', middleware);
+app.get(['/', '/home', '/about', '/contact-me'], middleware);
 app.get('*', (req, res) => {
   res.statusCode = 404;
   middleware(req, res);
